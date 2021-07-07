87060898e6f384d128840264c1fb35db732e8650
yanlifeng <yanlifeng19981116@qq.com>
2021-07-07 09:19:28 +0800

    init, sample count lines and bases based on RabbitIO

---
+++ cmake-build-release/CMakeFiles/RabbitQCPlus.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ylf9811/CLionProjects/RabbitQCPlus/src/FastxStream.cpp" "/Users/ylf9811/CLionProjects/RabbitQCPlus/cmake-build-release/CMakeFiles/RabbitQCPlus.dir/src/FastxStream.cpp.o"
+  "/Users/ylf9811/CLionProjects/RabbitQCPlus/src/Formater.cpp" "/Users/ylf9811/CLionProjects/RabbitQCPlus/cmake-build-release/CMakeFiles/RabbitQCPlus.dir/src/Formater.cpp.o"
+  "/Users/ylf9811/CLionProjects/RabbitQCPlus/src/cmdinfo.cpp" "/Users/ylf9811/CLionProjects/RabbitQCPlus/cmake-build-release/CMakeFiles/RabbitQCPlus.dir/src/cmdinfo.cpp.o"
+  "/Users/ylf9811/CLionProjects/RabbitQCPlus/src/main.cpp" "/Users/ylf9811/CLionProjects/RabbitQCPlus/cmake-build-release/CMakeFiles/RabbitQCPlus.dir/src/main.cpp.o"
+  "/Users/ylf9811/CLionProjects/RabbitQCPlus/src/peqc.cpp" "/Users/ylf9811/CLionProjects/RabbitQCPlus/cmake-build-release/CMakeFiles/RabbitQCPlus.dir/src/peqc.cpp.o"
+  "/Users/ylf9811/CLionProjects/RabbitQCPlus/src/seqc.cpp" "/Users/ylf9811/CLionProjects/RabbitQCPlus/cmake-build-release/CMakeFiles/RabbitQCPlus.dir/src/seqc.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
